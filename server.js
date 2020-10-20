b2c377425a236a24ee5baa66fde0a9fbe80e0835
Putter V <putter173@icloud.com>
2020-10-20 08:34:40 +0700

    Added Heroku Port Support

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const session = require('express-session')
 const mysql = require('mysql');
 const con = mysql.createPool({
